05af8573c65f3444a6489dd74aec369e515b92c5
almusto <ales.musto@gmail.com>
2017-02-01 10:59:23 -0500

    goin shoppin

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -6,7 +6,60 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
+
 class Cart {
+
+  var items = [Item]()
+
+  func totalPriceInCents() -> Int {
+    var totalCost : Int = 0
+    for item in items {
+      totalCost += item.priceInCents
+    }
+    return totalCost
+  }
+
+  func add(item: Item) {
+    items.append(item)
+  }
+
+  func remove(item: Item) {
+    if let index = items.index(of: item) {
+    items.remove(at: index)
+    }
+  }
+
+  func items(withName name : String) -> [Item] {
+    var newItems = [Item]()
+    for item in items {
+      if item.name == name {
+        newItems.append(item)
+      }
+    }
+    return newItems
+  }
+
+
+  func items(withMinPrice price: Int) -> [Item] {
+    var newItems = [Item]()
+    for item in items {
+      if item.priceInCents >= price {
+        newItems.append(item)
+      }
+    }
+    return newItems
+  }
+
+  func items(withMaxPrice price: Int) -> [Item] {
+    var newItems = [Item]()
+    for item in items {
+      if item.priceInCents <= price {
+        newItems.append(item)
+      }
+    }
+    return newItems
+  }
+
     
     
 }
